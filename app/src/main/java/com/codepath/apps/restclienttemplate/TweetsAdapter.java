121d4f01eb1d78945e6b16e55e7ef3e50d25fec1
Helen Paulini <hpaulini@fb.com>
2020-06-29 18:19:20 -0400

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -0,0 +1,48 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+    //pass in the context and list of tweets
+
+    //for each row, inflate the layout for a tweet
+
+    //bind values based on the position of the element
+
+    //define a viewholder
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView ivProfileImage;
+        TextView tvBody;
+        TextView tvScreenName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
+            tvBody = itemView.findViewById(R.id.tvBody);
+            tvScreenName = itemView.findViewById(R.id.tvScreenName);
+
+        }
+    }
+}
