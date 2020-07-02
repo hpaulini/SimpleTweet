9b7279a7718d22d80a354172f1f897cea0cd7153
Helen Paulini <hpaulini@fb.com>
2020-07-02 12:01:32 -0400

    reformatted date, time

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -113,20 +113,23 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
         }
 
         public String getRelativeTimeAgo(String rawJsonDate) {
-            String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
-            SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
-            sf.setLenient(true);
+            String formattedTime = TimeFormatter.getTimeDifference(rawJsonDate);
+            return formattedTime;
 
-            String relativeDate = "";
-            try {
-                long dateMillis = sf.parse(rawJsonDate).getTime();
-                relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
-                        System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS, DateUtils.FORMAT_ABBREV_RELATIVE).toString();
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-
-            return relativeDate;
+//            String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+//            SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+//            sf.setLenient(true);
+//
+//            String relativeDate = "";
+//            try {
+//                long dateMillis = sf.parse(rawJsonDate).getTime();
+//                relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+//                        System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS, DateUtils.FORMAT_ABBREV_RELATIVE).toString();
+//            } catch (ParseException e) {
+//                e.printStackTrace();
+//            }
+//
+//            return relativeDate;
         }
 
     }
